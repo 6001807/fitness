cf536931fc2ee2fe5fda2e1e44a32626012f9a2a
JEsper <6001807@mborijnland.nl>
2024-04-25 15:35:29 +0200

    login

---
+++ static/classes/class.app.js
@@ -0,0 +1,24 @@
+export class Application {
+    constructor(loggedIn) {
+        this.loggedIn = loggedIn;
+        document.addEventListener("DOMContentLoaded", () => {
+            this.updateDom();
+            console.log("app started");
+        })
+    }
+
+    updateDom() {
+        const logIn = document.querySelector(".login");
+        const logOut = document.querySelector(".logout");
+        const profile = document.querySelector(".profile");
+        const register = document.querySelector(".register");
+
+        if(this.loggedIn) {
+            logIn.style.display = "none";
+            register.style.display = "none";
+        } else {
+            logOut.style.display = "none";
+            profile.style.display = "none";
+        }
+    }
+}
---
+++ static/script/main.js
@@ -0,0 +1,3 @@
+import { Application } from '../classes/class.app.js';
+
+const app = new Application(true);
