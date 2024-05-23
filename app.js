fde4a1b1202bed49d6face5b0974273f44f1b571
JEsper <6001807@mborijnland.nl>
2024-05-23 14:46:35 +0200

    Revert "Merge branch 'master' of https://github.com/6001807/fitness"
    This reverts commit 17d290bf763441511d22ef7dcbb9bf12f2a17a8f, reversing
    changes made to 8d5cf949c307ae028db455f05ee01ca56be864a6.

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-import express, { json } from "express";
-import apiRouter from "./routes/api.js";
-import userRouter from "./routes/user.js";
-import session from "express-session";
+import express, { json } from 'express';
+import api from './routes/api.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(json());
-
-app.use(express.static("static"));
-
-app.use(
-    session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: true },
-    })
-);
-
-app.use("/api", apiRouter);
-app.use("/user", userRouter);
+app.use(express.static('static'));
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
+
+app.use('/api', api);
